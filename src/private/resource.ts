43cc16a37be77f346b303239f3e235ff92ff8220
DevonArtmeier <devon.artmeier@outlook.com>
2024-01-07 15:31:07 -0500

    Removed unnecessary virtual functions

--- src/private/resource.ts
+++ src/private/resource.ts
@@ -5,12 +5,6 @@ export class Resource
 {
 	// Constructor
 	protected constructor(protected _context: Context, public readonly id: string) { }
-	
-	// Bind
-	public bind?(): void;
-	
-	// Unbind
-	public unbind?(): void;
 		
 	// Delete
 	public delete?(): void;
